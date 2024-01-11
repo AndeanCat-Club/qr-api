6f14e1f3f3b2c69be4c35e9db8b30f07f5b4dc6e
eliasNav <elias.navarrete@powerbe.cl>
2024-01-11 05:18:08 -0300

    feat: cors

--- main.go
+++ main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 
-	"qr-api/qr-generator/routes"
+	route "qr-api/qr-generator/routes"
+
+	"github.com/gin-contrib/cors"
 )
 
 func main() {
 	router := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"*"}
+	config.AllowMethods = []string{"GET", "POST", "OPTIONS"}
+	config.AllowHeaders = []string{"Origin", "Content-Type", "Content-Length", "Accept-Encoding", "Authorization"}
+	config.ExposeHeaders = []string{"Content-Length"}
+
+	router.Use(cors.New(config))
+
 	route.RoutesConfig(router)
 	router.Run(":8080")
 }
